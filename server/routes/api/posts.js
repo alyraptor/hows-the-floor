73c85137e0a3b3a000afef4e0de793b09d6b5a5a
Aly Richardson <alyraptor101@gmail.com>
2019-07-13 17:11:50 -0500

    Connected to Mongo; Create and delete posts

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+// Add post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+// Delete post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://aly:developmentraptor@cluster0-iuvlt.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+
+    return client.db('Cluster0').collection('posts');
+}
+
+
+
+module.exports = router;
